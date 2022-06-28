fdaab7fdc25b280c5510060917e0d4ed12d7274e
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-28 14:16:10 -0400

    feat: create Dwarf,Elf,Halfling and Orc classes in src/Races/

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  private _maxLifePoints:number;
+  private static _createdRacesInstances = 0;
+
+  constructor(name:string, dexterity:number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc._createdRacesInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this._createdRacesInstances;
+  }
+}
+
+// https://www.typescripttutorial.net/typescript-tutorial/typescript-static-methods-and-properties/
--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,7 @@
 import Race from './Race';
 
 export default Race;
+export { default as Elf } from './Elf';
+export { default as Dwarf } from './Dwarf';
+export { default as Orc } from './Orc';
+export { default as Halfling } from './Halfling';
