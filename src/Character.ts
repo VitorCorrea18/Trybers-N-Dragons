8ad53253bf1ce327fb485aec716b95f93f03bcc0
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-28 20:07:05 -0400

    fix: grammar and getters

--- src/Character.ts
+++ src/Character.ts
@@ -19,7 +19,7 @@ export default class Character implements Fighter {
     this._name = name;
     this._dexterity = getRandomInt(1, 10);
     this._race = new Elf(this._name, this._dexterity);
-    this._archetype = new Mage(name);
+    this._archetype = new Mage(this._name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
     this._strength = getRandomInt(1, 10);
